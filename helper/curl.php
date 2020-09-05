f6f941ebd18b5f036f6fc38cad26150ca3a66679
WuJianwu <7594870087@qq.com>
2020-09-05 22:37:15 +0800

    curl请求不直接输出返回内容

--- helper/curl.php
+++ helper/curl.php
@@ -47,7 +47,6 @@ class curl
         }
         //如果是本地或开发环境，使用代理访问
 //        if(in_array(env(), ['local', 'dev'])){
-//            $this->setOption(CURLOPT_RETURNTRANSFER, true);
 //            $this->setOption(CURLOPT_HTTPPROXYTUNNEL, true);
 //            $this->setOption(CURLOPT_PROXYAUTH, CURLAUTH_BASIC);
 //            $this->setOption(CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
@@ -55,8 +54,8 @@ class curl
 //            $this->setOption(CURLOPT_PROXYPORT, '1080');
 //        }
 
+        $this->setOption(CURLOPT_RETURNTRANSFER, true);
         $this->setOption(CURLOPT_HTTPGET, true);
-        Log::info($this->buildUrl($url, $params));
         $response = $this->exec($this->buildUrl($url, $params));
         $this->close();
         return $response;
